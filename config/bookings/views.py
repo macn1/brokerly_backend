c64f89f965471639d2888e092ac29d2ec6384376
maccn1 <athulraihan27@gmail.com>
2025-11-24 16:33:55 +0530

    lead generetaion for vendor and admin completed

--- config/bookings/views.py
+++ config/bookings/views.py
@@ -4,8 +4,12 @@ from rest_framework.response import Response
 from authentication.models import User
 from apartments.models import Apartments
 from .models import LeadRequest
-from .serializers import LeadRequestSerializer
-
+from .serializer import LeadRequestSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from permissoins.super_permissions import IsDomainAdmin
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 class LeadCreateAPIView(APIView):
 
     def post(self, request):
@@ -27,7 +31,6 @@ class LeadCreateAPIView(APIView):
             )
         data["user"] = user.id
 
-        # Step 3: Save lead
         serializer = LeadRequestSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -35,5 +38,44 @@ class LeadCreateAPIView(APIView):
                 {"message": "Lead submitted successfully", "data": serializer.data},
                 status=201
             )
-
         return Response(serializer.errors, status=400)
+
+class PaginatedLeadPageapi(APIView):
+    permission_classes=[IsAuthenticated]
+    def get(self,request):
+        leads = LeadRequest.objects.all()
+        paginator = PageNumberPagination()
+        paginator.page_size=10
+        result_page = paginator.paginate_queryset(leads,request)
+        serializer = LeadRequestSerializer(result_page,many=True)
+        return paginator.get_paginated_response(serializer.data)
+    
+
+
+class PaginatedLeadPageapiVendor(APIView):
+    permission_classes = [IsAuthenticated, IsDomainAdmin]
+
+    def get(self, request):
+        domain = request.domain  
+        leads = LeadRequest.objects.filter(apartment__domain=domain)
+
+        print('leads',leads)
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        result_page = paginator.paginate_queryset(leads, request)
+
+        serializer = LeadRequestSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+class ReadUpdateDeleleLeads(APIView):
+    permission_classes=[IsAuthenticated]
+    
+    def get (self,request,pk):
+        leads = get_object_or_404(LeadRequest,pk=pk)
+        serialzier = LeadRequestSerializer(leads)
+        return Response(serialzier.data,status=status.HTTP_200_OK)
+    
+    def delete(self,request,pk):
+        leads = get_object_or_404(LeadRequest,pk=pk)
+        leads.delete()
+        return Response({"message","item deteted"},status=status.HTTP_204_NO_CONTENT)
